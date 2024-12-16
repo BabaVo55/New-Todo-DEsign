1fdee6b2dd86a1c0f5105a1474bc9a15fa1d892f
BabaVo55 <ethemcode@outlook.com>
2024-12-16 16:50:11 +0000

    try

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,14 @@ function App() {
 
   const [todos, setTodos] = useState(['tod', 'setTodos','randomg']);
 
-  function handleAddTodos(newTodo){
-    const newList = [...todos, newTodo];
-    setTodos(newList)
-
   return (
     <div className='main'>
       <div className='h1'>
         <h1>TodoList broski</h1>
       </div>
 
-      <TodoInput todos={todos} setTodos={setTodos} handleAddTodos={handleAddTodos} />
-      <TodoList tododos={todos}/>
+      <TodoInput />
+      <TodoList todos={too}/>
 
     </div>
   )
